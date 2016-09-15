ec58bce191c6e27cbb19d01818041140cba756e3
Arthur Xavier <arthur.xavierx@gmail.com>
2016-09-15 20:14:37 -0300

    Remove 'Main.purs'; do React bootstrap on index.js instead

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ require('./node_modules/todomvc-common/base.js');
 require('./node_modules/todomvc-common/base.css');
 require('./node_modules/todomvc-app-css/index.css');
 
-var Main = require('./src/Main.purs');
-Main["main"]();
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Container = require('./src/Components/Container.purs').containerComponent;
+
+ReactDOM.render(React.createElement(Container, {}, []), document.getElementById('app'));
